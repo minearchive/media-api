03842e5a56aa21ab7fda05a7741ee1e7d1c4d426
minechanjp <102400088+minechanjp@users.noreply.github.com>
2024-03-07 13:58:12 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use j4rs::prelude::jobject;
+use j4rs::Jvm;
+use j4rs::prelude::ptr;
+use j4rs::prelude::c_void;
+use j4rs::prelude::JNIEnv;
+use j4rs_derive::call_from_java;
+use windows::Media::Control::{GlobalSystemMediaTransportControlsSession, GlobalSystemMediaTransportControlsSessionManager};
+
+#[call_from_java("com.minearchive.WinAPI.tryStart")]
+fn try_start() {
+    let _ = get_session().TryPlayAsync();
+}
+
+#[call_from_java("com.minearchive.WinAPI.tryStop")]
+fn try_stop() {
+    let _ = get_session().TryStopAsync();
+}
+
+#[call_from_java("com.minearchive.WinAPI.trySkipNext")]
+fn try_skip_next() {
+    let _ = get_session().TrySkipNextAsync();
+}
+
+#[call_from_java("com.minearchive.WinAPI.trySkipPrevious")]
+fn try_skip_previous() {
+    let _ = get_session().TrySkipPreviousAsync();
+}
+
+fn get_session() -> GlobalSystemMediaTransportControlsSession {
+    let manager = GlobalSystemMediaTransportControlsSessionManager::RequestAsync().unwrap();
+    return manager.get().unwrap().GetCurrentSession().unwrap();
+}
